ba832248f7aa3db2101cc07eb7e354b32b3c2659
mariog44 <mg441236@gmail.com>
2024-04-25 11:19:24 -0500

    Se refresco el proyecto para que se agregue el modelo de conexion

---
+++ Models/ModeloConexion.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ALQUILER_VEHICULOS.Models
+{
+    public class ModeloConexion
+    {
+
+        public static MySqlConnection Conect()
+        {
+            String Servidor = "localhost";
+            String BaseDatos = "alquiler_vehiculos";
+            String Usuario = "root";
+            String Contrasena = "juan123";
+            String CadenaConexion = "database = " + BaseDatos + "; Data Source = " + Servidor + "; User Id= " + Usuario + "; Password = " + Contrasena;
+
+            try
+            {
+                MySqlConnection ConexionBD = new(CadenaConexion);
+                return ConexionBD;
+            }
+            catch (MySqlException)
+            {
+                return null;
+            }
+        }
+
+        public static Boolean ExecuteNonQuerySentence(String ConsultaSQL)
+        {
+            Boolean Bandera = false;
+            MySqlConnection Conexion = Conect();
+            Conexion.Open();
+            try
+            {
+                MySqlCommand Comando = new(ConsultaSQL, Conexion);
+                Comando.ExecuteNonQuery();
+                Bandera = true;
+            }
+            catch (MySqlException error)
+            {
+                Bandera = false;
+                Console.WriteLine(error);
+            }
+            finally
+            {
+                Conexion.Close();
+            }
+            return Bandera;
+        }
+    }
+}
